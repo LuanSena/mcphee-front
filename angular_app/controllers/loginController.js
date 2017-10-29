2ad5857010674bd8448fe87a76b8e2ae49e4ad8f
Luan Sena <luansena@live.com>
2017-10-29 14:51:22 -0200

    implements rootScope broadCast

--- angular_app/controllers/loginController.js
+++ angular_app/controllers/loginController.js
@@ -1,43 +1,48 @@
 angular.module('mainApp')
-.controller('loginController',
-['$http', '$scope', 'showHideService', 'BACKEND_API', function ($http, $scope, showHideService, BACKEND_API) {
-    /*Login screen vars*/
-    $scope.email = '';
-    $scope.password = '';
-    $scope.loginMsg = '';
-    $scope.loading = false;
-    /*Request user and pass then set userToken*/
-    $scope.requestLogin = function () {
-        $scope.loginMsg = 'Verificando credenciais...';
-        $scope.loading = true;
+    .controller('loginController',
+        ['$http', '$scope', 'showHideService', 'mainPageService', 'BACKEND_API', '$rootScope',
+            function ($http, $scope, showHideService, mainPageService, BACKEND_API, $rootScope) {
+            /*Login screen vars*/
+            $scope.email = '';
+            $scope.password = '';
+            $scope.loginMsg = '';
+            $scope.loading = false;
+            /*Request user and pass then set userToken*/
+            $scope.requestLogin = function () {
+                $scope.loginMsg = 'Verificando credenciais...';
+                $scope.loading = true;
 
-        $http({
-            method: 'POST',
-            url: BACKEND_API + 'v1/auth',
-            headers: {'Content-Type': 'application/json', 'user': $scope.email,
-                'password': $scope.password},
-            data: {
-                'email': $scope.email,
-                'password': $scope.password
-            }
-        }).then
-        (function success(response) {
-            if (response.status === 200) {
-                $scope.loginMsg = '';
-                $scope.loading = false;
-                showHideService.setShowLogin(false);
-                showHideService.setShowMain(true);
+                $http({
+                    method: 'POST',
+                    url: BACKEND_API + 'v1/auth',
+                    headers: {
+                        'Content-Type': 'application/json', 'user': $scope.email,
+                        'password': $scope.password
+                    },
+                    data: {
+                        'email': $scope.email,
+                        'password': $scope.password
+                    }
+                }).then
+                (function success(response) {
+                    if (response.status === 200) {
+                        $scope.loginMsg = '';
+                        $scope.loading = false;
+                        showHideService.setShowLogin(false);
+                        showHideService.setShowMain(true);
+                        mainPageService.setSourceData(response.data);
+                        $rootScope.$broadcast('TreeReload')
 
-            }
-        }, function error(response) {
+                    }
+                }, function error(response) {
 
-            if (response.status === 401) {
-                $scope.loading = false;
-                $scope.loginMsg = 'Usuário e/ou senha inválido!';
-                console.log('Fail to login, status:'+ response.status) ;
-            } else {
-                $scope.loginMsg = 'Serviço indisponível, contate o administrador ('+ response.status +')';
-            }
-        })
-    };
-}]);
+                    if (response.status === 401) {
+                        $scope.loading = false;
+                        $scope.loginMsg = 'Usuário e/ou senha inválido!';
+                        console.log('Fail to login, status:' + response.status);
+                    } else {
+                        $scope.loginMsg = 'Serviço indisponível, contate o administrador (' + response.status + ')';
+                    }
+                })
+            };
+        }]);

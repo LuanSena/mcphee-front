dcd01ae222510806fe8c814949ecf1d2f57e950b
Luan Sena <lbarbosa@stone.com.br>
2017-11-09 21:59:11 -0200

    implements working diary post

--- angular_app/controllers/createDiaryController.js
+++ angular_app/controllers/createDiaryController.js
@@ -1,32 +1,37 @@
 angular.module('mainApp')
-    .controller('createDiaryController', ['$http', '$rootScope', '$scope', '$timeout', function ($http, $scope, $rootScope, $timeout) {
-        $scope.diary_text = 'Insira aqui o seu texto';
-        $scope.selected_class_id = 'nenhuma!';
-        $scope.classes = ['Carregando...'];
+    .controller('createDiaryController', ['$http', '$rootScope', '$scope', 'BACKEND_API', function ($http, $scope, $rootScope, BACKEND_API) {
+        $scope.diary = {
+            text: 'Insira aqui o seu texto',
+            classes: ['Carregando...'],
+            selected: 0
+        };
 
-        $scope.postClassDiary = function (param) {
-            // $scope.loginMsg = 'Verificando credenciais...';
-            // $scope.loading = true;
-            //
-            // $http({
-            //     method: 'POST',
-            //     url: BACKEND_API + 'v1/class-diary',
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     data: {
-            //         'diaryText': $scope.diary_text
-            //     }
-            // }).then
-            // (function success(response) {
-            //     if (response.status === 200) {
-            //         $scope.diary_text = '';
-            //         alert("Diário publicado com sucesso!")
-            //     }
-            // }, function error(response) {
-            //     console.log(response);
-            //     alert("Ocorreu um erro a o publicar o diário");
-            // })
+        $scope.postClassDiary = function () {
+            if ($scope.diary.selected > 0) {
+                alert($scope.diary.text + $scope.diary.selected);
+                $http({
+                    method: 'POST',
+                    url: BACKEND_API + 'v1/diary/class/' + $scope.diary.selected,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        'diaryText': $scope.diary.text
+                    }
+                }).then
+                (function success(response) {
+                    if (response.status === 202) {
+                        $scope.diary.text = '';
+                        alert("Diário publicado com sucesso!")
+                    }
+                }, function error(response) {
+                    console.log(response);
+                    alert("Ocorreu um erro a o publicar o diário");
+                })
+            }
+            else {
+                alert("É necessário selecionar uma turma primeiro")
+            }
         };
 
         $rootScope.$on('getPersonClasses', function (event, args) {
